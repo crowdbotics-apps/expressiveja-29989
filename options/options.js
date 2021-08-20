c67552debe0a7b0c6e3fb376500b2943f7019c6e
Crowdbotics <team@crowdbotics.com>
2021-08-20 22:01:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "expressiveja_29989Identifier",
+  url: "https://expressiveja_29989Identifier.botics.co",
+  api: "https://expressiveja_29989Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
